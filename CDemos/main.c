8198c6ddb4179611aca570b7089c7579ca48dd1c
zenghao0708 <zenghao0708@gmail.com>
2016-11-06 11:07:33 +0800

    Initial Commit

---
+++ CDemos/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CDemos
+//
+//  Created by zenghao on 2016/11/6.
+//  Copyright © 2016年 zenghao0708. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
